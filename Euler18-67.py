8b25627826f8929db98a14b7e33d89392284daa6
TheTrigun99 <139170389+TheTrigun99@users.noreply.github.com>
2023-07-24 23:49:00 +0200

    Create Euler18-67.py

---
+++ Euler18-67.py
@@ -0,0 +1,29 @@
+b="""75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+def transfotrig(n):
+    trian = []
+    for i in n.split("\n"):
+        L=[]
+        for j in i.split():
+            L.append(int(j))
+        trian.append(L)
+    return trian
+def maxpathtri(n):
+    trian=transfotrig(n)
+    for i in range(len(trian)-1,0,-1):
+        for j in range(i):
+            trian[i-1][j]=trian[i-1][j]+max(trian[i][j],trian[i][j+1])
+    return trian[0][0]
